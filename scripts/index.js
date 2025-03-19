1d8874b87255dffaf034f319b18119a11b863297
Potato. <105754685+dragon731012@users.noreply.github.com>
2025-03-19 13:18:59 -0500

    e

--- scripts/index.js
+++ scripts/index.js
@@ -136,6 +136,7 @@ const createScene = async () => {
     
         // Get gun tip position in world space
         let gunTipPosition = showbullet.getAbsolutePosition();
+        let spawnOffset = bulletDirection.scale(-0.2);
         bullet.position.copyFrom(gunTipPosition); // Place the bullet exactly at the gun tip
     
         // Get the gun's forward direction
