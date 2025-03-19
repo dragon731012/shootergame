e4eb60bf69fa6367f23618be2207d6d9354e75f5
Potato. <105754685+dragon731012@users.noreply.github.com>
2025-03-19 12:32:11 -0500

    e

--- scripts/index.js
+++ scripts/index.js
@@ -125,7 +125,7 @@ const createScene = async () => {
     document.addEventListener("click", () => {
         Explode(showbullet,0.01,0.02);
 
-        var bullet = add3d("assets/bullet.glb");
+        var bullet = showbullet.clone();
         bullet.isVisible=false;
         bullet.scaling=new BABYLON.Vector3(0.1, 0.1, 0.1);
         const bulletMaterial = new BABYLON.StandardMaterial("bulletMaterial", scene);
