3d4b2b0cf351820178056133f082aabc4a0b0244
Potato. <105754685+dragon731012@users.noreply.github.com>
2025-03-19 12:36:25 -0500

    e

--- scripts/index.js
+++ scripts/index.js
@@ -134,7 +134,7 @@ const createScene = async () => {
         hl.addMesh(bullet, BABYLON.Color3.Yellow());
 
         bullet.parent=gun;
-        bullet.position=showbullet.position;
+        bullet.position=new BABYLON.Vector3(-0.9, 0.14, 1.65);
         bullet.rotation = new BABYLON.Vector3(0, 0, 0);
         bullet.material = bulletMaterial;
     
