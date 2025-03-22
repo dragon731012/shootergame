134cc98524d7b2e89268f72673a0762eef724eaf
Potato. <105754685+dragon731012@users.noreply.github.com>
2025-03-22 11:28:10 -0500

    e

--- scripts/client.js
+++ scripts/client.js
@@ -124,7 +124,7 @@ window.handleOtherPlayerMovement = function(data) {
                 data.rotationData.y,
                 data.rotationData.z
             );
-            const yaw = Math.atan2(-dir.x, dir.z)-Math.PI/2;
+            const yaw = Math.atan2(-dir.x, dir.z);
             remote.model.rotationQuaternion = BABYLON.Quaternion.RotationYawPitchRoll(yaw, 0, 0);
         }
 
