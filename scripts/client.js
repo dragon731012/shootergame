7778d7f984e54c9ec99a4ce587e1a5e8172b0f0a
Potato. <105754685+dragon731012@users.noreply.github.com>
2025-03-22 14:15:53 -0500

    e

--- scripts/client.js
+++ scripts/client.js
@@ -110,8 +110,8 @@ window.handleOtherPlayerShoot = async function(data) {
 
     bullet.parent=remote.model;
 
-    bullet.position=new BABYLON.Vector3(0.1,1.3,1);
-    bullet.rotation=new BABYLON.Vector3(Math.PI,0,0);
+    bullet.position=new BABYLON.Vector3(0.1,1.4,1);
+    bullet.rotation=new BABYLON.Vector3(0,Math.PI,0);
 
     // Detach bullet from remote player's model to move independently.
     bullet.setParent(null);
